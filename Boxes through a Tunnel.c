91f61fbfc371a780cf226c3fdf0a1d76c73766e8
MIRACULOUS65 <sushovan1908@gmail.com>
2025-01-01 15:33:05 +0530

    All the codes did to achieve 5 star in HackerRank

---
+++ Boxes through a Tunnel.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_HEIGHT 41
+
+struct box {
+    int length;
+    int width;
+    int height;
+};
+
+typedef struct box box;
+
+int get_volume(box b) {
+    // Calculate and return the volume of the box
+    return b.length * b.width * b.height;
+}
+
+int is_lower_than_max_height(box b) {
+    // Check if the box's height is lower than MAX_HEIGHT
+    return b.height < MAX_HEIGHT ? 1 : 0;
+}
+
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	box *boxes = malloc(n * sizeof(box));
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
+	}
+	for (int i = 0; i < n; i++) {
+		if (is_lower_than_max_height(boxes[i])) {
+			printf("%d\n", get_volume(boxes[i]));
+		}
+	}
+	return 0;
+}
